dce8f772f52311b008dad59afec95b1d9b562e3a
observator0722 <observator0722@gmail.com>
2024-01-14 20:56:16 +0100

    add dropdown

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -1,11 +1,31 @@
-import './SearchInput.css'
+import './SearchInput.scss'
 import { MdSearch } from "react-icons/md";
+import { useState } from 'react';
 
 const SearchInput = () => {
-  return <div className='SearchInput'>
-    <MdSearch className='SearchIcon' col />
-    <input placeholder='Введите поисковый запрос' />
-  </div>
+  const [value, setValue] = useState('');
+
+  const handleValueChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return (
+    <div className='SearchInput Extended'>
+      <MdSearch className='SearchIcon' col />
+      <input placeholder='Введите поисковый запрос' value={value} onChange={handleValueChange} />
+      <div className='SearchTipsList'>
+        <div className='SearchTip'>
+          <span>Test</span>
+        </div>
+        <div className='SearchTip'>
+          <span>Test</span>
+        </div>
+        <div className='SearchTip'>
+          <span>Test</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SearchInput
