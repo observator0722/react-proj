6e64a7a3363845dd8206191df9304da3e855b80b
observator0722 <observator0722@gmail.com>
2024-01-15 13:11:38 +0100

    Options

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -4,28 +4,49 @@ import { useState } from 'react';
 
 const SearchInput = () => {
   const [value, setValue] = useState('');
+  const [extended, setExtended] = useState(false)
+  const [options, setOptions] = useState([])
 
   const handleValueChange = (event) => {
     setValue(event.target.value)
   }
 
+  const handleInputFocus = (event) => {
+    setExtended(true)
+  }
+
+  const handleInputBlur = (event) => {
+    setExtended(false)
+  }
+
   return (
     <div className='SearchInput Extended'>
-      <MdSearch className='SearchIcon' col />
-      <input placeholder='Введите поисковый запрос' value={value} onChange={handleValueChange} />
-      <div className='SearchTipsList'>
-        <div className='SearchTip'>
-          <span>Test</span>
-        </div>
-        <div className='SearchTip'>
-          <span>Test</span>
-        </div>
-        <div className='SearchTip'>
-          <span>Test</span>
-        </div>
-      </div>
+      <MdSearch className='SearchIcon' />
+      <input placeholder='Введите поисковый запрос'
+        value={value}
+        onChange={handleValueChange}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        onSubmit={e => {
+          setOptions([value, ...options]);
+          setValue('')}} />
+      {
+        extended
+          ? <div className='SearchTipsList'>
+            {
+              options.map(o => <SearchTip value={o}/>)
+            }
+          </div>
+          : <></>
+      }
     </div>
   )
+}
+
+const SearchTip = (props) => {
+  return <div className='SearchTip'>
+    <span>{props.value}</span>
+  </div>
 }
 
 export default SearchInput
