433865de096104c89332811628c3392b90fe1ca4
observator0722 <observator0722@gmail.com>
2024-01-29 18:26:39 +0100

    formik

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,17 +1,29 @@
 import { Box, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react"
+import { useFormik } from "formik";
 
 const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    }
+  });
+
   return (
     <Box display='flex' justifyContent='center' alignItems='center' flexDir='column'>
       <Box w={500}>
         <FormControl isInvalid={false} mb={5}>
           <FormLabel>Email</FormLabel>
-          <Input type='email' placeholder='Enter your email' />
+          <Input name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            type='email'
+            placeholder='Enter your email' />
           <FormErrorMessage></FormErrorMessage>
         </FormControl>
         <FormControl isInvalid={false}>
           <FormLabel>Password</FormLabel>
-          <Input placeholder='Enter your password' />
+          <Input name="password" value={formik.values.password} onChange={formik.handleChange} placeholder='Enter your password' />
           <FormErrorMessage></FormErrorMessage>
         </FormControl>
       </Box>
