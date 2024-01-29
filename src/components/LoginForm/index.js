1897882570a0c4612fb0a5ee8bf819af0dd8c417
observator0722 <observator0722@gmail.com>
2024-01-29 21:03:59 +0100

    validate email

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,25 +1,33 @@
 import { Box, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react"
 import { useFormik } from "formik";
+import * as Yup from 'yup'
 
 const LoginForm = () => {
   const formik = useFormik({
     initialValues: {
       email: '',
       password: ''
-    }
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Email is required').email('Email is incorrect'),
+      password: Yup.string().required('Password is required').min(8, 'Min length for password is 8 characters')
+    })
   });
+
+  console.log(formik);
 
   return (
     <Box display='flex' justifyContent='center' alignItems='center' flexDir='column'>
       <Box w={500}>
-        <FormControl isInvalid={false} mb={5}>
+        <FormControl isInvalid={!!formik.errors.email} mb={5}>
           <FormLabel>Email</FormLabel>
           <Input name="email"
             value={formik.values.email}
             onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             type='email'
             placeholder='Enter your email' />
-          <FormErrorMessage></FormErrorMessage>
+          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
         </FormControl>
         <FormControl isInvalid={false}>
           <FormLabel>Password</FormLabel>
