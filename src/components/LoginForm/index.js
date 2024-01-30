840cf06e9942e5ad3ace2050d5b04eaf057cfd72
observator0722 <observator0722@gmail.com>
2024-01-30 20:30:07 +0100

    formik

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,4 @@
-import { Box, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react"
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react"
 import { useFormik } from "formik";
 import * as Yup from 'yup'
 
@@ -11,15 +11,23 @@ const LoginForm = () => {
     validationSchema: Yup.object({
       email: Yup.string().required('Email is required').email('Email is incorrect'),
       password: Yup.string().required('Password is required').min(8, 'Min length for password is 8 characters')
-    })
+    }),
+    validateOnMount: true
   });
 
-  console.log(formik);
+  const handleSubmit = () => {
+    if (!formik.isValid) {
+      alert('login Failed')
+      return
+    }
+    
+    alert(JSON.stringify(formik.values))
+  }
 
   return (
     <Box display='flex' justifyContent='center' alignItems='center' flexDir='column'>
-      <Box w={500}>
-        <FormControl isInvalid={!!formik.errors.email} mb={5}>
+      <Box w={500} mb={4}>
+        <FormControl isInvalid={!!formik.errors.email && formik.touched.email} mb={5}>
           <FormLabel>Email</FormLabel>
           <Input name="email"
             value={formik.values.email}
@@ -27,14 +35,27 @@ const LoginForm = () => {
             onBlur={formik.handleBlur}
             type='email'
             placeholder='Enter your email' />
-          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+          {
+            !!formik.errors.email && formik.touched.email
+              ? <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              : <></>
+          }
         </FormControl>
-        <FormControl isInvalid={false}>
+        <FormControl isInvalid={!!formik.errors.password && formik.touched.password}>
           <FormLabel>Password</FormLabel>
-          <Input name="password" value={formik.values.password} onChange={formik.handleChange} placeholder='Enter your password' />
-          <FormErrorMessage></FormErrorMessage>
+          <Input name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder='Enter your password' />
+          {
+            !!formik.errors.password && formik.touched.password
+              ? <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+              : <></>
+          }
         </FormControl>
       </Box>
+      <Button onClick={handleSubmit}>Login</Button>
     </Box>
   )
 }
